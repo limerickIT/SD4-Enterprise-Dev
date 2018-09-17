a9a2d53bc43e2a95a0c6e659ddf0110491a8454b
lit-alan <Alan.Ryan@lit.ie>
2018-09-17 10:17:18 +0100

    Servlets Lecture Added

---
+++ Lectures/L3 Servlets/ServletSource/src/java/UsingPOST/UsingPostMethod.java
@@ -0,0 +1,39 @@
+package UsingPOST;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class UsingPostMethod extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter( "nameField" );
+        String yob = request.getParameter( "yobField" );
+
+        int year = Integer.parseInt(yob);
+        int age = 2008 - year;
+
+        try {
+
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Using Get</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("Hello " + name + ", welcome to servlets!");
+            out.println("<br>");
+            out.println("You are " + age + " years old");
+            out.println("</body>");
+            out.println("</html>");
+
+        } finally {
+            out.close();
+        }
+
+    }
+
+
+}
