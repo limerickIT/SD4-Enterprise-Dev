54ab6dd211687d18d622855cee2e7e83effb477d
lit-alan <Alan.Ryan@lit.ie>
2018-09-25 19:22:47 +0100

    Starter Code For Lab Ex2 Added

---
+++ Lectures/Lab Exercises/Exercise Two - Using JSTL/StarterCode/src/java/src/model/Title.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.model;
+
+/**
+ *
+ * @author Alan.Ryan
+ */
+public class Title {
+    private String ISBN;
+    private String title;
+    private String editionNumber;
+    private int yearPublished;
+    private int publisherID;
+    private double price;
+
+    public Title(String ISBN, String title, String editionNumber, int yearPublished, int publisherID, double price) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.editionNumber = editionNumber;
+        this.yearPublished = yearPublished;
+        this.publisherID = publisherID;
+        this.price = price;
+    }
+
+    /**
+     * @return the ISBN
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the editionNumber
+     */
+    public String getEditionNumber() {
+        return editionNumber;
+    }
+
+    /**
+     * @param editionNumber the editionNumber to set
+     */
+    public void setEditionNumber(String editionNumber) {
+        this.editionNumber = editionNumber;
+    }
+
+    /**
+     * @return the yearPublished
+     */
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    /**
+     * @param yearPublished the yearPublished to set
+     */
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    /**
+     * @return the publisherID
+     */
+    public int getPublisherID() {
+        return publisherID;
+    }
+
+    /**
+     * @param publisherID the publisherID to set
+     */
+    public void setPublisherID(int publisherID) {
+        this.publisherID = publisherID;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+     
+}
---
+++ Lectures/Lab Exercises/Exercise Two - Using JSTL/StarterCode/web/index.jsp
@@ -0,0 +1,26 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/functions" prefix = "fn" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>All Titles</title>
+    </head>
+    
+    
+    
+    
+    <!-- In this example you need to run the servlet (DisplayAllTitles) first and not the index page -->
+    
+    
+    
+    <h3> Number of Titles Returned  ${fn:length(titles)} </h3>
+
+        <c:forEach var="aTitle" items="${titles}">
+            ${aTitle.title} <br>
+        </c:forEach>
+    
+    
+</html>
