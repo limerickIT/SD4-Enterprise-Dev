f76a6ba13b927ed8460ffcb5a754ee03422e59b5
lit-alan <Alan.Ryan@lit.ie>
2018-09-25 16:18:14 +0100

    Updated Solution Added

---
+++ Lectures/Lab Exercises/Exercise One - Handle Form Submission/UpdatedSoln/src/java/data/CustomerIO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import beans.Customer;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Alan.Ryan
+ */
+public class CustomerIO {
+    
+    public static void add(Customer c, String filepath) throws IOException {
+
+        System.out.println("writes to file");
+//        File file = new File(filepath);
+//        PrintWriter out = new PrintWriter(new FileWriter(file, true));
+//        out.println(c.getName() + "|" + c.getAddress() + "|" + c.getCity() + "|" +c.getCountry() + "|" + c.getPhone() + "|" + c.getComments());
+//        out.close();
+//        
+    }
+    
+}
---
+++ Lectures/Lab Exercises/Exercise One - Handle Form Submission/UpdatedSoln/src/java/servlets/HandleWriteToFile.java
@@ -0,0 +1,84 @@
+package servlets;
+
+import beans.Customer;
+import data.CustomerIO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Alan.Ryan
+ */
+@WebServlet(name = "HandleWriteToFile", urlPatterns = {"/HandleWriteToFile"})
+public class HandleWriteToFile extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+                  
+            HttpSession session = request.getSession();
+            ServletContext sc = this.getServletContext();
+            String path = sc.getRealPath("CustomerList.txt");
+            Customer c= (Customer) session.getAttribute("customer");
+            CustomerIO.add(c, path);
+            session.invalidate();
+            
+            RequestDispatcher dispatcher = request.getRequestDispatcher("thanks.jsp");
+            dispatcher.forward(request, response);
+
+            
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
