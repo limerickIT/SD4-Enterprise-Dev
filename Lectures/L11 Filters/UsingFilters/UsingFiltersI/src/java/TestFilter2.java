1ef6f7ab8fe831262f5723d70934f6433f90ca37
lit-alan <Alan.Ryan@lit.ie>
2018-11-05 10:44:36 +0000

    L11 Filters Added

---
+++ Lectures/L11 Filters/UsingFilters/UsingFiltersI/src/java/TestFilter2.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Alan.Ryan
+ */
+public class TestFilter2 implements Filter {
+    
+    private FilterConfig filterConfig = null;
+    
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+     
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        ServletContext sc = filterConfig.getServletContext();
+        
+        String filterName = filterConfig.getFilterName();
+        String servletPath = "Servlet Path " + httpRequest.getServletPath();
+        
+        sc.log(filterName  + " | " + servletPath + " | before request ");
+        
+        chain.doFilter(request, response);
+        
+        sc.log(filterName  + " | " + servletPath + " | after request ");
+        
+        
+    }//end doFilter
+     
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {   
+        
+        filterConfig = null;
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+    
+}
